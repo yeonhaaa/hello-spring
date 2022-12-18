d0259f859d349b6d3344440c3e52b03a8f92fec4
JW <jiwon@hongjiwon-ui-MacBookAir.local>
2022-12-18 18:29:21 +0900

    add: domain, repository 생성

---
+++ src/main/java/com/yh/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,35 @@
+package com.yh.hellospring.repository;
+
+import com.yh.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put((member.getId()), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id)); // null값 가능
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny(); // 하나 찾으면 바로 반환. 끝까지 돌려도 없으면 null 이라도 반환함.
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}
