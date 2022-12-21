e363a2b43b27b97aaca611af5692c460e571bffd
JW <jiwon@hongjiwon-ui-MacBookAir.local>
2022-12-22 00:16:04 +0900

    feat: register member name, connect to h2 db

---
+++ src/main/java/com/yh/hellospring/controller/MemberForm.java
@@ -0,0 +1,17 @@
+package com.yh.hellospring.controller;
+
+public class MemberForm {
+
+    private String name;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
