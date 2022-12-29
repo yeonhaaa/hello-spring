c686d20390708ebdf08a31bcf104176aa36fe5ed
yeonha.hong <qkdxo101@naver.com>
2022-12-29 14:04:50 +0900

    update: config

--- src/main/java/com/yh/hellospring/SpringConfig.java
+++ src/main/java/com/yh/hellospring/SpringConfig.java
@@ -1,14 +1,25 @@
 package com.yh.hellospring;
 
+import com.yh.hellospring.repository.JdbcMemberRepository;
 import com.yh.hellospring.repository.MemberRepository;
 import com.yh.hellospring.repository.MemoryMemberRepository;
 import com.yh.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
@@ -16,6 +27,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
